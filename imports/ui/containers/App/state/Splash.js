a6fee5b61e5fbf9dd1e6995f36bb0fc5da1ba3f2
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-02-15 11:26:00 -0800

    added splash screen pause

--- imports/ui/containers/App/state/Splash.js
+++ imports/ui/containers/App/state/Splash.js
@@ -2,9 +2,9 @@ import Phaser from '/imports/startup/phaser-split'
 import { centerGameObjects } from '../utils'
 
 export default class extends Phaser.State {
-  init () {}
+  init() { }
 
-  preload () {
+  preload() {
     // this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
     // this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
     // centerGameObjects([this.loaderBg, this.loaderBar])
@@ -21,8 +21,16 @@ export default class extends Phaser.State {
     this.load.audio('bulletFire', ['audio/bulletFire.mp3']);
   }
 
-  create () {
-    this.state.start('Game')
+  create() {
+
+    this.goToGame = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    let text = this.add.text(this.world.centerX, this.world.centerY-500, 'Welcome to play game, press space and play for fun.', { font: '35px Arial', fill: '#000', align: 'center' })
+
   }
 
+  update() {
+    if (this.goToGame.isDown) {
+      this.state.start('Game');
+    }
+  }
 }
