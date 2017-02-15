3f8a2b2fd877cdad3dcb192a89d647185fb87bde
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-02-15 14:51:49 -0800

    fullscreened game

--- imports/ui/containers/App/state/Boot.js
+++ imports/ui/containers/App/state/Boot.js
@@ -1,17 +1,17 @@
 import Phaser from '/imports/startup/phaser-split'
 
 export default class extends Phaser.State {
-  init () {
+  init() {
     this.stage.backgroundColor = '#EDEEC9'
   }
 
-  preload () {
+  preload() {
     let text = this.add.text(this.world.centerX, this.world.centerY, 'Hi there', { font: '16px Arial', fill: '#dddddd', align: 'center' })
     text.anchor.setTo(0.5, 0.5);
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   }
 
-  render () {
-      this.state.start('Splash')
+  render() {
+    this.state.start('Splash')
   }
 }
