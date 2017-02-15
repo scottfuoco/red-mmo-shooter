139b84c4648e97f37a661b0ddcd2b4b8c0728d9a
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-02-15 11:05:54 -0800

    code clean up

--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -11,17 +11,17 @@ import config from './config'
 class Game extends Phaser.Game {
 
   constructor() {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
+    const docElement = document.documentElement;
+    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth;
+    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight;
 
-    super(width, height, Phaser.AUTO, 'game', null)
+    super(width, height, Phaser.AUTO, 'game', null);
 
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
-    this.state.add('Game', GameState, false)
+    this.state.add('Boot', BootState, false);
+    this.state.add('Splash', SplashState, false);
+    this.state.add('Game', GameState, false);
 
-    this.state.start('Boot')
+    this.state.start('Boot');
   }
 }
 
